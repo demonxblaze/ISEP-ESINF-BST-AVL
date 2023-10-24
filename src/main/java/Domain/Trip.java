884629c0b3a5fb466ccfd85ff0022b19d6d86853
Ricardo <1220606@isep.ipp.pt>
2023-10-24 16:32:01 +0100

    Add method to Trip Changed EX4 class

--- src/main/java/Domain/Trip.java
+++ src/main/java/Domain/Trip.java
@@ -35,6 +35,14 @@ public class Trip implements Comparable<Trip>{
         setLongitudeEnd(longitudeEnd);
     }
 
+    public Trip(int vehID, int tripID, double latitudeStart, double longitudeStart, double latitudeEnd, double longitudeEnd) {
+        this.vehID = vehID;
+        this.tripID = tripID;
+        this.latitudeStart = latitudeStart;
+        this.longitudeStart = longitudeStart;
+        this.latitudeEnd = latitudeEnd;
+        this.longitudeEnd = longitudeEnd;
+    }
 
     public Trip(double dayNum, int vehID, int tripID, double timestamp, double latitudeStart, double longitudeStart,
                 double latitudeEnd, double longitudeEnd, double speed, String maf, String rpm, String load,
@@ -260,6 +268,31 @@ public class Trip implements Comparable<Trip>{
         this.longitudeEnd = longitudeEnd;
     }
 
+    public double getTripDistance(){
+
+        // Code extracted from https://www.geeksforgeeks.org/program-distance-two-points-earth/
+
+        double lon1 = Math.toRadians(longitudeStart);
+        double lon2 = Math.toRadians(longitudeEnd);
+        double lat1 = Math.toRadians(latitudeStart);
+        double lat2 = Math.toRadians(latitudeEnd);
+
+        // Haversine formula
+        double dlon = lon2 - lon1;
+        double dlat = lat2 - lat1;
+        double a = Math.pow(Math.sin(dlat / 2), 2)
+                + Math.cos(lat1) * Math.cos(lat2)
+                * Math.pow(Math.sin(dlon / 2),2);
+
+        double c = 2 * Math.asin(Math.sqrt(a));
+
+        // Radius of earth in kilometers. Use 3956
+        // for miles
+        double r = 6371;
+
+        // calculate the result
+        return(c * r);
+    }
     @Override
     public int hashCode() {
         return tripID;
--- src/main/java/EX4.java
+++ src/main/java/EX4.java
@@ -1,2 +1,46 @@
+import Domain.Trip;
+import Trees.AVL;
+
+import java.util.List;
+
 public class EX4 {
+
+    private AVL<Trip> insertDataInTheTree(List<String[]> lines) {
+        AVL<Trip> trips = new AVL<>();
+
+        double latitudeStart = 0;
+        double longitudeStart = 0;
+        double latitudeEnd = 0;
+        double longitudeEnd = 0;
+
+        int tripID = -1;
+
+
+
+        for (String[] s : lines) {
+
+
+
+            if (tripID == -1) {
+                tripID = Integer.parseInt(s[0]);
+                latitudeStart = Double.parseDouble(s[1]);
+                longitudeStart = Double.parseDouble(s[2]);
+
+            } else if ( tripID != Integer.parseInt(s[0])) {
+                trips.insert(new Trip(tripID, latitudeStart, longitudeStart, latitudeEnd, longitudeEnd));
+                tripID = Integer.parseInt(s[0]);
+                latitudeStart = Double.parseDouble(s[1]);
+                longitudeStart = Double.parseDouble(s[2]);
+                latitudeEnd =0;
+                longitudeEnd = 0;
+
+            }
+            latitudeEnd = Double.parseDouble(s[1]);
+            longitudeEnd = Double.parseDouble(s[2]);
+
+        }
+
+        trips.insert(new Trip(tripID, latitudeStart, longitudeStart, latitudeEnd, longitudeEnd));
+
+        return trips;
 }
