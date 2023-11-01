2584652955b21de1108bebc1d81d1c3012f34b65
Diogo Campos <darc.main.84@outlook.pt>
2023-11-01 10:28:55 +0000

    Class KDTREE

--- src/main/java/Trees/KDTREE.java
+++ src/main/java/Trees/KDTREE.java
@@ -6,12 +6,13 @@ import org.w3c.dom.Node;
 import java.awt.geom.Point2D;
 import java.util.Comparator;
 
-public class KDTREE<E extends Comparable<E>> extends BST<E> {
+public class KDTREE<E  extends Comparable<E>> extends BST<E> {
 
+    protected TwoDNode<E> root;
 
-/*
     // Nested static class for a 2DTree node.
     protected class TwoDNode<E> extends Node<E>  {
+        protected E info;
         protected Point2D.Double coords;
         protected TwoDNode<E> left;
         protected TwoDNode<E> right;
@@ -44,8 +45,18 @@ public class KDTREE<E extends Comparable<E>> extends BST<E> {
         public void setRight(TwoDNode<E> right) {
             this.right = right;
         }
-    }
 
+        public void setObject(TwoDNode<E> node) {
+            this.info = node.info;
+            this.coords = node.coords;
+            this.left = node.left;
+            this.right = node.right;
+        }
+
+    }
+        public KDTREE() {
+            root= new TwoDNode<E>(null,null,null,null);
+        }
         private final Comparator<TwoDNode<E>> cmpX = new Comparator<TwoDNode<E>>(){
             @Override
             public int compare(TwoDNode<E> p1, TwoDNode<E> p2) {
@@ -58,23 +69,57 @@ public class KDTREE<E extends Comparable<E>> extends BST<E> {
                 return Double.compare(p1.getY(), p2.getY());
             }
         };
+/*
+    @Override
+    public void insert(E element) {
+        if (element == null) return;
+        Point2D.Double coords = element.getCoords();
+        root = insert( root, new TwoDNode<>(element,coords,null,null), true);
+    }
 
-    public TwoDNode<E> insert(TwoDNode<E> currentNode, TwoDNode<E> node,
-                            boolean divX){
-        if (node.coords.equals(currentNode.coords))
-            return currentNode;
-        int cmpResult = (divX ? cmpX : cmpY).compare(node, currentNode);
-        if (cmpResult == -1)
-            if (currentNode.left == null)
-                currentNode.setLeft(node);
-            else
-                insert(currentNode.getLeft(), node, !divX);
-        else
-        if (currentNode.right == null)
-            currentNode.setRight(node);
-        else
-            insert(currentNode.right, node, !divX);
-    }*/
+    private TwoDNode<E> insert(TwoDNode<E> currentNode, TwoDNode<E> node,
+                               boolean divX){
+        if (currentNode == null) {
+            return node;
+        }
+        if (divX) {
+            if (cmpX.compare(node, currentNode) < 0) {
+                currentNode.setLeft(insert(currentNode.getLeft(), node, !divX));
+            } else {
+                currentNode.setRight(insert(currentNode.getRight(), node, !divX));
+            }
+        } else {
+            if (cmpY.compare(node, currentNode) < 0) {
+                currentNode.setLeft(insert(currentNode.getLeft(), node, !divX));
+            } else {
+                currentNode.setRight(insert(currentNode.getRight(), node, !divX));
+            }
+        }
 
 
-}
+
+
+    }
+
+    public E findNearestNeighbour(TwoDNode<E> node, double x, double y,
+                                   TwoDNode<E> closestNode, boolean divX){
+        if (node == null)
+            return null;
+        double d = Point2D.distanceSq(node.coords.x, node.coords.y, x, y);
+        double closestDist = Point2D.distanceSq(closestNode.coords.x,
+                closestNode.coords.y, x, y);
+        if (closestDist > d) {
+            closestNode.setObject(node);
+            double delta = divX ? x - node.coords.x : y - node.coords.y;
+            double delta2 = delta * delta;
+            TwoDNode<E> node1 = delta < 0 ? node.left : node.right;
+            TwoDNode<E> node2 = delta < 0 ? node.right : node.left;
+            findNearestNeighbour(node1, x, y, closestNode, !divX);
+            if (delta2 < closestDist) {
+                findNearestNeighbour(node2, x, y, closestNode, !divX);
+                return closestNode.info;
+            }
+        }
+*/
+    }
+
